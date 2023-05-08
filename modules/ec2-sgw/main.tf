014efa3597f37746469cb84d04fc2d20194c1ca6
EC2 Default User <ec2-user@ip-172-16-2-236.corp.surampa.com>
2023-05-08 04:12:24 +0000

    EC2 SGW Module

---
+++ modules/ec2-sgw/main.tf
@@ -0,0 +1,53 @@
+##########################
+## Create EC2 Instance ##
+##########################
+
+resource "aws_instance" "ec2-sgw" {
+  ami                    = data.aws_ami.sgw-ami.id
+  vpc_security_group_ids = [module.ec2_sg.security_group_id]
+  subnet_id              = var.subnet_id
+  instance_type          = var.instance_type
+  key_name               = aws_key_pair.my_key_pair.key_name
+  ebs_optimized          = true
+
+  root_block_device {
+    encrypted   = true
+    volume_type = "gp3"
+    volume_size = var.root_disk_size
+  }
+  tags = {
+    Name = var.name
+  }
+}
+
+data "aws_ami" "sgw-ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["aws-storage-gateway-*"]
+  }
+}
+
+resource "aws_eip" "ip" {
+  instance = aws_instance.ec2-sgw.id
+  vpc      = true
+}
+
+resource "aws_key_pair" "my_key_pair" {
+  key_name   = var.ssh_key_name
+  public_key = file("${abspath(path.cwd)}/${var.ssh_key_name}.pub")
+}
+
+resource "aws_volume_attachment" "ebs_volume" {
+  device_name = "/dev/sdb"
+  volume_id   = aws_ebs_volume.cache-disk.id
+  instance_id = aws_instance.ec2-sgw.id
+}
+
+resource "aws_ebs_volume" "cache-disk" {
+  availability_zone = aws_instance.ec2-sgw.availability_zone
+  size              = var.cache_disk_size
+  type              = "gp3"
+}
---
+++ modules/ec2-sgw/sg.tf
@@ -0,0 +1,70 @@
+module "ec2_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "${var.name}.security-group"
+  description = "Security group for user-service with custom ports open within VPC, and PostgreSQL publicly open"
+  vpc_id      = var.vpc_id
+
+  ingress_cidr_blocks = [var.ingress_cidr_blocks]
+  ingress_rules       = ["ssh-tcp"]
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      description = "HTTP required only for activation purposes. The port is closed after activation."
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      from_port   = 2049
+      to_port     = 2049
+      protocol    = "tcp"
+      description = "NFS-TCP"
+      cidr_blocks = var.ingress_cidr_blocks
+    },
+    {
+      from_port   = 111
+      to_port     = 111
+      protocol    = "tcp"
+      description = "NFS"
+      cidr_blocks = var.ingress_cidr_blocks
+    },
+    {
+      from_port   = 20048
+      to_port     = 20048
+      protocol    = "tcp"
+      description = "NFSv3-TCP"
+      cidr_blocks = var.ingress_cidr_blocks
+    },
+    {
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      description = "HTTPS"
+      cidr_blocks = var.ingress_cidr_blocks
+    },
+    {
+      from_port   = 53
+      to_port     = 53
+      protocol    = "tcp"
+      description = "DNS-TCP"
+      cidr_blocks = var.ingress_cidr_blocks
+    },
+    {
+      from_port   = 123
+      to_port     = 123
+      protocol    = "udp"
+      description = "NTP"
+      cidr_blocks = var.ingress_cidr_blocks
+    },
+    {
+      from_port   = 445
+      to_port     = 445
+      protocol    = "tcp"
+      description = "SMB"
+      cidr_blocks = var.ingress_cidr_blocks
+    }
+  ]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules       = ["all-all"]
+}
