ff4e0785ab41557c6eb1c28bfa7c79fd9000a4ba
prabirsekhri <sekhrip@amazon.com>
2023-07-05 20:24:12 -0400

    added VPC flow log config to ec2-nfs example

--- examples/s3-nfs-filegateway-ec2/main.tf
+++ examples/s3-nfs-filegateway-ec2/main.tf
@@ -66,6 +66,8 @@ module "ec2-sgw" {
 
 data "aws_availability_zones" "available" {}
 
+#VPC flow logs enabled. Skipping tfsec bug https://github.com/aquasecurity/tfsec/issues/1941
+#tfsec:ignore:aws-ec2-require-vpc-flow-logs-for-all-vpcs
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
@@ -76,9 +78,12 @@ module "vpc" {
   public_subnets  = [for subnet in range(var.subnet-count) : cidrsubnet(var.vpc_cidr_block, 8, sum([subnet, var.subnet-count]))]
   name            = "${random_pet.name.id}-gateway-vpc"
 
-  enable_dns_hostnames = true
-  create_igw           = true
-
+  enable_dns_hostnames                 = true
+  create_igw                           = true
+  enable_flow_log                      = true
+  create_flow_log_cloudwatch_log_group = true
+  create_flow_log_cloudwatch_iam_role  = true
+  flow_log_max_aggregation_interval    = 60
 }
 
 ###################################
