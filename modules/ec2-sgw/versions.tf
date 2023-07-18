8bf537dcc0c577e70869c976ea9e42ad560562f7
prabirsekhri <sekhrip@amazon.com>
2023-07-18 15:14:28 -0400

    changing resources to snakecase and updating examples

--- modules/ec2-sgw/versions.tf
+++ modules/ec2-sgw/versions.tf
@@ -1,12 +1,13 @@
 terraform {
+  required_version = ">= 1.0.7"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.0"
+      version = ">= 4.0.0"
+    }
+    awscc = {
+      source  = "hashicorp/awscc"
+      version = ">= 0.24.0"
     }
   }
-}
-
-provider "aws" {
-  region = var.aws_region
 }
