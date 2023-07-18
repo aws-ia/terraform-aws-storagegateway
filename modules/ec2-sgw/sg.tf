2641b3cb62035279e1ae0c009153b8c833fe5685
prabirsekhri <sekhrip@amazon.com>
2023-07-18 15:58:23 -0400

    refactored sg for ec2 module

--- modules/ec2-sgw/sg.tf
+++ modules/ec2-sgw/sg.tf
@@ -1,5 +1,6 @@
 locals {
   ingress_cidr_blocks_list           = split(",", var.ingress_cidr_blocks)
+  egress_cidr_blocks_list            = split(",", var.egress_cidr_blocks)
   ingress_cidr_block_activation_list = split(",", var.ingress_cidr_block_activation)
 }
 
@@ -10,105 +11,162 @@ resource "aws_security_group" "ec2_sg" {
   name        = "${var.name}.security-group"
   description = "Security group with custom ports open within VPC for client connectivity and communication with AWS."
   vpc_id      = var.vpc_id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    description = "HTTP required only for activation purposes. The port is closed after activation."
-    cidr_blocks = local.ingress_cidr_block_activation_list
-  }
-  ingress {
-    from_port   = 2049
-    to_port     = 2049
-    protocol    = "tcp"
-    description = "NFS-TCP"
-    cidr_blocks = local.ingress_cidr_blocks_list
-  }
-  ingress {
-    from_port   = 2049
-    to_port     = 2049
-    protocol    = "udp"
-    description = "NFS-UDP"
-    cidr_blocks = local.ingress_cidr_blocks_list
-  }
-  ingress {
-    from_port   = 111
-    to_port     = 111
-    protocol    = "tcp"
-    description = "NFS"
-    cidr_blocks = local.ingress_cidr_blocks_list
-  }
-  ingress {
-    from_port   = 111
-    to_port     = 111
-    protocol    = "udp"
-    description = "NFS"
-    cidr_blocks = local.ingress_cidr_blocks_list
-  }
-  ingress {
-    from_port   = 20048
-    to_port     = 20048
-    protocol    = "tcp"
-    description = "NFSv3-TCP"
-    cidr_blocks = local.ingress_cidr_blocks_list
-  }
-  ingress {
-    from_port   = 20048
-    to_port     = 20048
-    protocol    = "udp"
-    description = "NFSv3-UDP"
-    cidr_blocks = local.ingress_cidr_blocks_list
-  }
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    description = "HTTPS"
-    cidr_blocks = local.ingress_cidr_blocks_list
-  }
-  ingress {
-    from_port   = 53
-    to_port     = 53
-    protocol    = "tcp"
-    description = "DNS-TCP"
-    cidr_blocks = local.ingress_cidr_blocks_list
-  }
-  ingress {
-    from_port   = 123
-    to_port     = 123
-    protocol    = "udp"
-    description = "NTP"
-    cidr_blocks = local.ingress_cidr_blocks_list
-  }
-  ingress {
-    from_port   = 139
-    to_port     = 139
-    protocol    = "tcp"
-    description = "SMB over NetBIOS"
-    cidr_blocks = local.ingress_cidr_blocks_list
-  }
-  ingress {
-    from_port   = 139
-    to_port     = 139
-    protocol    = "udp"
-    description = "SMB over NetBIOS"
-    cidr_blocks = local.ingress_cidr_blocks_list
-  }
-  ingress {
-    from_port   = 445
-    to_port     = 445
-    protocol    = "tcp"
-    description = "SMB"
-    cidr_blocks = local.ingress_cidr_blocks_list
-  }
-  #outbound connections for Storage Gateway for activation
-  #tfsec:ignore:aws-ec2-no-public-egress-sgr 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    description = "Storage Gateway egress traffic for activation"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
+
+resource "aws_security_group_rule" "http" {
+  for_each          = var.create_security_group == true ? toset(["ec2_sg"]) : toset([])
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  description       = "HTTP required only for activation purposes. The port is closed after activation."
+  cidr_blocks       = local.ingress_cidr_block_activation_list
+  security_group_id = aws_security_group.ec2_sg["ec2_sg"].id
+}
+
+resource "aws_security_group_rule" "nfs_tcp" {
+  for_each          = var.create_security_group == true ? toset(["ec2_sg"]) : toset([])
+  type              = "ingress"
+  from_port         = 2049
+  to_port           = 2049
+  protocol          = "tcp"
+  description       = "NFS-TCP"
+  cidr_blocks       = local.ingress_cidr_blocks_list
+  security_group_id = aws_security_group.ec2_sg["ec2_sg"].id
+}
+
+resource "aws_security_group_rule" "nfs_udp" {
+  for_each          = var.create_security_group == true ? toset(["ec2_sg"]) : toset([])
+  type              = "ingress"
+  from_port         = 2049
+  to_port           = 2049
+  protocol          = "udp"
+  description       = "NFS-UDP"
+  cidr_blocks       = local.ingress_cidr_blocks_list
+  security_group_id = aws_security_group.ec2_sg["ec2_sg"].id
+}
+
+resource "aws_security_group_rule" "nfs_portmapper_tcp" {
+  for_each          = var.create_security_group == true ? toset(["ec2_sg"]) : toset([])
+  type              = "ingress"
+  from_port         = 111
+  to_port           = 111
+  protocol          = "tcp"
+  description       = "NFS-portmapper-TCP"
+  cidr_blocks       = local.ingress_cidr_blocks_list
+  security_group_id = aws_security_group.ec2_sg["ec2_sg"].id
+}
+
+resource "aws_security_group_rule" "nfs_portmap_udp" {
+  for_each          = var.create_security_group == true ? toset(["ec2_sg"]) : toset([])
+  type              = "ingress"
+  from_port         = 111
+  to_port           = 111
+  protocol          = "udp"
+  description       = "NFS-portmapper-UDP"
+  cidr_blocks       = local.ingress_cidr_blocks_list
+  security_group_id = aws_security_group.ec2_sg["ec2_sg"].id
+}
+
+
+resource "aws_security_group_rule" "nfs_v3_tcp" {
+  for_each          = var.create_security_group == true ? toset(["ec2_sg"]) : toset([])
+  type              = "ingress"
+  from_port         = 20048
+  to_port           = 111
+  protocol          = "tcp"
+  description       = "NFSv3-TCP"
+  cidr_blocks       = local.ingress_cidr_blocks_list
+  security_group_id = aws_security_group.ec2_sg["ec2_sg"].id
+}
+
+resource "aws_security_group_rule" "nfs_v3_udp" {
+  for_each          = var.create_security_group == true ? toset(["ec2_sg"]) : toset([])
+  type              = "ingress"
+  from_port         = 20048
+  to_port           = 111
+  protocol          = "UDP"
+  description       = "NFSv3-UDP"
+  cidr_blocks       = local.ingress_cidr_blocks_list
+  security_group_id = aws_security_group.ec2_sg["ec2_sg"].id
+}
+
+resource "aws_security_group_rule" "https" {
+  for_each          = var.create_security_group == true ? toset(["ec2_sg"]) : toset([])
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  description       = "HTTPS"
+  cidr_blocks       = local.ingress_cidr_blocks_list
+  security_group_id = aws_security_group.ec2_sg["ec2_sg"].id
+}
+
+resource "aws_security_group_rule" "dns_tcp" {
+  for_each          = var.create_security_group == true ? toset(["ec2_sg"]) : toset([])
+  type              = "ingress"
+  from_port         = 53
+  to_port           = 53
+  protocol          = "tcp"
+  description       = "DNS-TCP"
+  cidr_blocks       = local.ingress_cidr_blocks_list
+  security_group_id = aws_security_group.ec2_sg["ec2_sg"].id
+}
+
+resource "aws_security_group_rule" "ntp_udp" {
+  for_each          = var.create_security_group == true ? toset(["ec2_sg"]) : toset([])
+  type              = "ingress"
+  from_port         = 123
+  to_port           = 123
+  protocol          = "udp"
+  description       = "NTP-UDP"
+  cidr_blocks       = local.ingress_cidr_blocks_list
+  security_group_id = aws_security_group.ec2_sg["ec2_sg"].id
+}
+
+resource "aws_security_group_rule" "smb_netbios_tcp" {
+  for_each          = var.create_security_group == true ? toset(["ec2_sg"]) : toset([])
+  type              = "ingress"
+  from_port         = 139
+  to_port           = 139
+  protocol          = "tcp"
+  description       = "SMB over NetBIOS - TCP"
+  cidr_blocks       = local.ingress_cidr_blocks_list
+  security_group_id = aws_security_group.ec2_sg["ec2_sg"].id
+}
+
+resource "aws_security_group_rule" "smb_netbios_udp" {
+  for_each          = var.create_security_group == true ? toset(["ec2_sg"]) : toset([])
+  type              = "ingress"
+  from_port         = 139
+  to_port           = 139
+  protocol          = "udp"
+  description       = "SMB over NetBIOS - UDP"
+  cidr_blocks       = local.ingress_cidr_blocks_list
+  security_group_id = aws_security_group.ec2_sg["ec2_sg"].id
+}
+
+resource "aws_security_group_rule" "smb_tcp" {
+  for_each          = var.create_security_group == true ? toset(["ec2_sg"]) : toset([])
+  type              = "ingress"
+  from_port         = 445
+  to_port           = 455
+  protocol          = "tcp"
+  description       = "SMB"
+  cidr_blocks       = local.ingress_cidr_blocks_list
+  security_group_id = aws_security_group.ec2_sg["ec2_sg"].id
+}
+
+#outbound connections for Storage Gateway for activation
+#tfsec:ignore:aws-ec2-no-public-egress-sgr 
+resource "aws_security_group_rule" "egress" {
+  for_each          = var.create_security_group == true ? toset(["ec2_sg"]) : toset([])
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  description       = "Storage Gateway egress traffic for activation"
+  cidr_blocks       = local.egress_cidr_blocks_list
+  security_group_id = aws_security_group.ec2_sg["ec2_sg"].id
+}
+
